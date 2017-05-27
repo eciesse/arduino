361e7de059e808d632b5e84e893dcf497db5fb86
eciesse <enrico.santus@gmail.com>
2017-05-27 16:42:26 +0200

    moved files

--- sketch_may26a/sketch_may26a.ino
+++ sketch_may26a/sketch_may26a.ino
@@ -1,24 +1,30 @@
 int photocellPin = 0;     // the cell and 10K pulldown are connected to a0
 int photocellReading;     // the analog reading from the sensor divider
 int LEDpin = 9;          // connect Red LED to pin 11 (PWM pin)
-int LEDbrightness;        // 
+int LEDbrightness;        //
+int LEDnight; 
+
 void setup(void) {
   // We'll send debugging information via the Serial monitor
   Serial.begin(9600);   
+  //pinMode(LEDpin, INPUT_PULLUP);
 }
  
 void loop(void) {
   photocellReading = analogRead(photocellPin);  
  
-  Serial.print("Analog reading = ");
-  Serial.println(photocellReading);     // the raw analog reading
+  Serial.print("ledbrightness = ");
+ // Serial.println(photocellReading);     // the raw analog reading
  
   // LED gets brighter the darker it is at the sensor
   // that means we have to -invert- the reading from 0-1023 back to 1023-0
   //photocellReading = 500 - photocellReading;
   //now we have to map 0-1023 to 0-255 since thats the range analogWrite uses
-  LEDbrightness = map(photocellReading, 0, 1023, 0, 255);
-  analogWrite(LEDpin, LEDbrightness);
+  LEDbrightness = map(photocellReading, 0, 400, 0, 255);
+  LEDnight=255-LEDbrightness;
+  if(LEDnight <80)LEDnight=0;
+  analogWrite(LEDpin, LEDnight);
+  Serial.println(LEDbrightness);
  
   delay(100);
 }
